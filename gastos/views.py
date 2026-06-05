8354dabb16c5d42b215d5ccf046822658e186ac9
Jorge Hernan Cuenca Marin <jorgebinkio@MacBook-Air-de-Jorge.local>
2026-06-05 11:56:40 -0500

    Fix: importa Count en gastos/views.py (NameError en reporte_gastos)

--- gastos/views.py
+++ gastos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.db.models import Sum, Q
+from django.db.models import Sum, Q, Count
 from django.http import HttpResponse
 from decimal import Decimal, InvalidOperation
 import openpyxl
